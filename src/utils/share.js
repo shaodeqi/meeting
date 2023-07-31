0b42fc2d6e1e1ab5a9f6454c4fd53697bc806aa0
邵德奇 <personalshao@163.com>
2023-07-31 16:14:38 +0800

    update

--- src/utils/share.js
+++ src/utils/share.js
@@ -67,8 +67,6 @@ export class MultiRTCPeerConnection extends EventTarget {
             'stun:stun.l.google.com:19302',
             'stun:stun1.l.google.com:19302',
             'stun:stun2.l.google.com:19302',
-            'stun:stun.l.google.com:19302?transport=udp',
-            'stun:stun.services.mozilla.com',
           ],
         },
       ],
